def83dd6f188f1b03a74acf64f3fb04b5324b71b
Jayanth <jayanth@jayanth.LOQ>
2026-09-19 12:29:33 +0530

    data file

--- react-stuff/src/App.jsx
+++ react-stuff/src/App.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-import EventSection from "./Components/Eventsection";
+import EventForm from "./Components/EventForm";
+import EventSection from "./Components/EventSection";
 import Footer from "./Components/footer";
+import { initialEvents } from "./data/events";
 
 function App() {
+  const [events, setEvents] = useState(initialEvents);
+
+  function handleAddEvent(newEvent) {
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+  }
+
   return (
-    <main className="app">
+    <div>
       <Navbar />
-      <Hero />
-      <EventSection />
+
+      <main id="home">
+        <Hero
+          title="Discover What Is Happening on Campus"
+          description="Find workshops, sports activities, club meetings, and opportunities to connect with other students."
+        />
+
+        <EventForm onAddEvent={handleAddEvent} />
+        <EventSection events={events} />
+      </main>
+
       <Footer />
-    </main>
+    </div>
   );
 }
 
 export default App;
+
